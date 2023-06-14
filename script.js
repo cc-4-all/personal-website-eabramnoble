5514f8a3a0637db287b32a7f2c0893f11e3b760f
eabramnoble <136475811+eabramnoble@users.noreply.github.com>
2023-06-14 15:40:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// if you have more images than me, type thier names in here...
+var images = ['https://th.bing.com/th/id/R.a67feb41230afc07c33c1bc9f566559e?rik=XtFakMJga0EI8g&riu=http%3a%2f%2fi.dailymail.co.uk%2fi%2fpix%2f2015%2f06%2f24%2f12%2f29ECCE5100000578-0-image-a-51_1435146050428.jpg&ehk=VDqllub27q9IgXViGjSpFTqIDdLlJdGVqu%2bPwLYWf5M%3d&risl=&pid=ImgRaw&r=0', 'https://i.pinimg.com/originals/65/e7/46/65e7461df1ec9966fc9246a2595e34d5.jpg', 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA15zQPO.img'];
+
+var currentimage = 0:
+
+function changeimage()
